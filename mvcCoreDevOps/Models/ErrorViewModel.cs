ef28424b3a2da355f1d2cc2cbd6c6ee0952609ba
vigneshwar447 <120728478+vigneshwar447@users.noreply.github.com>
2023-01-22 16:34:07 +0530

    First Checkin

---
+++ mvcCoreDevOps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvcCoreDevOps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
